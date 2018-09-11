588dd76361f9c61596bbf4e57114858d0df1fd21
gaoyafeng <yafeng.gao@ishansong.com>
2018-09-11 10:14:50 +0800

    优化了

--- LXD/LXMain/AppDelegate_BACKUP_41097.h
+++ LXD/LXMain/AppDelegate_BACKUP_41097.h
@@ -1,7 +1,11 @@
 //
 //  AppDelegate.h
 //  LXD
-//  APP代理3.0版本++1
+<<<<<<< HEAD
+//  APP代理2.0版本++1++2
+=======
+//  APP代理2.1版本++1++2
+>>>>>>> f005e34b90eae41c2f74e076566bb401bf48ff20
 //  Created by gaoyafeng on 2018/6/7.
 //  Copyright © 2018年 north_feng. All rights reserved.
 //
